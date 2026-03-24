0fffc38657e5dbbf308008509b17e80473ebf26a
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-03-23 22:13:22 -0300

    Service

---
+++ src/main/java/com/morotti/fornecedores/service/FornecedorService.java
@@ -0,0 +1,31 @@
+package com.morotti.fornecedores.service;
+
+import com.morotti.fornecedores.entity.FornecedoresEntity;
+import com.morotti.fornecedores.repository.FornecedoresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FornecedorService {
+    @Autowired
+    private FornecedoresRepository fornecedoresRepository;
+
+    public FornecedoresEntity criarFornecedor(FornecedoresEntity entity){
+        return fornecedoresRepository.save(entity);
+    }
+
+    public List<FornecedoresEntity> findAll(){
+        return fornecedoresRepository.findAll();
+    }
+
+    public FornecedoresEntity buscarPorId(Long id){
+        return fornecedoresRepository.findById(id).get();
+    }
+
+    public void deletar(Long id){
+        fornecedoresRepository.deleteById(id);
+    }
+    
+}
