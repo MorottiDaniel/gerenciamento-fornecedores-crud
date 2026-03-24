52fcb5e10804a56d1390d47c2b9c944711d5f11d
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-03-23 22:06:37 -0300

    Entity

---
+++ src/main/java/com/morotti/fornecedores/entity/FornecedoresEntity.java
@@ -0,0 +1,50 @@
+package com.morotti.fornecedores.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity(name = "Fornecedor")
+@Table(name = "tb_fornecedor")
+public class FornecedoresEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nomeFantasia;
+    private String cnpj;
+    @Column(name = "telefone")
+    private String contato;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
